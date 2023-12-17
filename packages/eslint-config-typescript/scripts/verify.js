f8d03f43e89091f4ebc80a09e8c8c78506bc2df9
Rune Klein <runeklein@outlook.com>
2023-12-17 19:17:06 +0100

    🔖 Bump

--- packages/eslint-config-typescript/scripts/verify.js
+++ packages/eslint-config-typescript/scripts/verify.js
@@ -9,68 +9,12 @@ try {
 		prefix: typescript.prefix
 	})
 
-	// Curated list
 	const rulesThatMustBeOff = [
-		'constructor-super',
-		'getter-return',
-		'no-const-assign',
-		'no-dupe-args',
-		'no-dupe-class-members',
-		'no-dupe-keys',
-		'no-func-assign',
-		'no-import-assign',
-		'no-new-symbol',
-		'no-obj-calls',
-		'no-redeclare',
-		'no-setter-return',
-		'no-this-before-super',
-		'no-undef',
-		'no-unreachable',
-		'no-unsafe-negation',
-		'valid-typeof',
-		'require-await',
-		'prefer-destructuring',
-		'no-useless-constructor',
-		'no-use-before-define',
-		'no-unused-vars',
-		'no-unused-expressions',
-		'no-throw-literal',
-		'no-shadow',
-		'no-restricted-imports',
-		'no-redeclare',
-		'no-magic-numbers',
-		'no-loss-of-precision',
-		'no-loop-func',
-		'no-invalid-this',
-		'no-implied-eval',
-		'no-empty-function',
-		'no-dupe-class-members',
-		'no-array-constructor',
-		'max-params',
-		'init-declarations',
-		'dot-notation',
-		'default-param-last',
-		'class-methods-use-this'
+		...Object.keys(typescript.getTypescriptBuiltinRules()),
+		...Object.keys(typescript.getTypescriptExtensionRules()).filter(
+			(key) => !key.startsWith(typescript.prefix)
+		)
 	]
-
-	console.log(
-		rulesFromTypescript.reduce((acc, rule) => {
-			if (rule.meta.docs.extendsBaseRule) {
-				const ebr = rule.meta.docs.extendsBaseRule
-				const name =
-					ebr === true
-						? rule.name.replace('@typescript-eslint/', '')
-						: ebr
-				const value = config.rules[name]
-
-				if (value !== undefined) {
-					return [...acc, name]
-				}
-			}
-
-			return acc
-		}, [])
-	)
 
 	const rulesThatMustExists = [
 		...rulesThatMustBeOff,
